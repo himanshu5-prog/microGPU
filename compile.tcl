990aa08a4d966d05e0aa5346a6b47343adbbb0bb
Himanshu Yadav <himanshu07319@gmail.com>
2026-02-10 01:30:07 -0800

    Added deprecated comment in compile.tcl

--- compile.tcl
+++ compile.tcl
@@ -1,3 +1,5 @@
+## Deprecated. Use make command instead. This file will not be updated. 
+## Please refer to Makefile for compilation instructions.
 g++ -std=c++17 -Wall -g -O0 -Wextra -pedantic \
 src/*.cc \
 src/computeUnit/*.cc \
