cdd66829cfbbdad89c2dd2a6eafd8aecbbe27674
Himanshu Yadav <himanshu07319@gmail.com>
2026-02-11 21:29:08 -0800

    ComputeUnit: Add comment to make code readable

--- src/computeUnit/computeUnit.hh
+++ src/computeUnit/computeUnit.hh
@@ -19,21 +19,37 @@ enum SMState {
 
 class ComputeUnit {
     std::vector<Warp> warps;
+
+    // Each compute unit has its own ID
     int smId;
+
+    // Warp ID which will be executed in next cycle
     int nextWarpId;
+
+    // Current warp ID being executed
     size_t currentWarpId;
+
+    // Current cycle
     int currentCycle;
+
+    // State of the compute unit
     SMState state;
 
     public:
         ComputeUnit() : nextWarpId(0), currentWarpId(0), currentCycle(0), state(SMState::IDLE) {}
-        
+       
+        // Setter methods
         void setState(SMState newState);
         void setCurrentWarpId(int warpId);
         void setWarp(const Warp &warp);
         void setSmId(int id) { smId = id; }
+
+        // Increment cycle count for the compute unit
         void incrementCycle() { currentCycle++; }
+
+        // Execute the current warp and advance its pipeline stage
         void execute(); 
+
         // Getter methods
         int getCurrentWarpId();
         SMState getState() const;
@@ -44,6 +60,8 @@ class ComputeUnit {
 
         // Print method for debugging
         void printId() const { std::cout << "(ComputeUnit) ComputeUnit ID: " << smId << std::endl; }
+
+        // Method to calculate the next warp ID to execute based on round-robin scheduling
         void calculateNextWarpId();
         
 };
